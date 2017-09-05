7985fd7dc5c5323c9ffb37720e7702dab5fa3896
Sungwoo Park <swpark@nexell.co.kr>
2017-09-05 16:47:49 +0900

    oreo: Fix build error in OREO
    assert() triggers build error in oreo so this patch comments out assert().
    
    Change-Id: I27bc01995223b394672a06edb67d19a802e022eb
    Signed-off-by: Sungwoo Park <swpark@nexell.co.kr>

--- drmeventlistener.cpp
+++ drmeventlistener.cpp
@@ -67,7 +67,7 @@ int DrmEventListener::Init()
 
 void DrmEventListener::RegisterHotplugHandler(DrmEventHandler *handler)
 {
-	assert(!hotplug_handler_);
+	// assert(!hotplug_handler_);
 	hotplug_handler_ = handler;
 }
 
